dc23a2918798792475318062e800ed7760796732
rb <32129393+Malrb@users.noreply.github.com>
2017-10-22 02:44:34 +0800

    Update php-magic2bypassMD5.php

--- php-magic2bypassMD5.php
+++ php-magic2bypassMD5.php
@@ -1,6 +1,6 @@
 <?php
 // 在 php 中， == 只有比較 “值"，但是 === 會連類型也一起比較
-// 而有趣的一點是，在 php 中，會把 md5 0E 開頭的 hash value 都當成 0
+// 而有趣的一點是，在 php 中，會把 md5 0E 開頭的 hash value 都當成 0 <- 0e科學記號=0
 // 接著看到 username="sena" 的那筆資料，發現他的密碼是 0E 開頭
 // 所以我只要找到一組 string 經過 md5 hashing 後會是 0E 就行了
 // 於是我找到了 240610708 這組 magic number 丟進去就拿到 flag 了XD
